1432ade65ffc0730c58afd9daba910d0a9ac0696
stopwatch <waghamaleshamika2013@gmail.com>
2019-09-14 20:57:13 +0530

    stop watch

--- app/src/main/java/com/example/content/stopwatch.java
+++ app/src/main/java/com/example/content/stopwatch.java
@@ -2,13 +2,18 @@ package com.example.content;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
 import android.os.Bundle;
+
 import android.os.SystemClock;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+
 import android.widget.Button;
+
 import android.widget.Chronometer;
 import android.widget.ImageView;
 
@@ -17,33 +22,50 @@ public class stopwatch extends AppCompatActivity {
     Button btnstart,btnstop;
     ImageView ivtick;
     Animation roundalone;
-    Chronometer timer;
+   Chronometer timer;
+    long pauseOffset;
 
-
-    @Override
+   boolean running;
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
 
-        btnstart=(Button)findViewById(R.id.btnBegin);
-        ivtick=(ImageView)findViewById(R.id.ivtick);
-        timer=(Chronometer)findViewById(R.id.timerhere);
-
-
+        btnstart=findViewById(R.id.btnBegin);
+        ivtick=findViewById(R.id.ivtick);
+        timer=(Chronometer)findViewById(R.id.timer);
+        btnstop=findViewById(R.id.btnstop);
 
         roundalone= AnimationUtils.loadAnimation(this,R.anim.roundalone);
 
-        btnstart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ivtick.startAnimation(roundalone);
+    }
 
-                timer.setBase(SystemClock.elapsedRealtime());
-                timer.start();
+    public void startTimer(View v){
+        if (!running){
+            timer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            timer.start();
+            running=true;
+            ivtick.startAnimation(roundalone);
 
-            }
-        });
+        }
+    }
+    public void pauseTimer(View v){
+        if(running){
+            timer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - timer.getBase();
+            running=false;
+            ivtick.clearAnimation();
+
+
+        }
+    }
+
+    public void stopTimer(View v){
+        timer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset=0;
+        ivtick.clearAnimation();
 
     }
+
 
 }
